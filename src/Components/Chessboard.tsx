953725aa33dd733a598fbdcd7b6872f8fee23d01
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-10-08 04:53:10 -0700

    add move functionality

--- src/Components/Chessboard.tsx
+++ src/Components/Chessboard.tsx
@@ -1,12 +1,20 @@
-import { Component, createSignal, For, splitProps } from "solid-js";
+import {
+  Component,
+  createEffect,
+  createSignal,
+  For,
+  splitProps,
+} from "solid-js";
 import { render } from "solid-js/web";
 import { PieceType, BoardSquare, defaultBoard, Piece } from "../defaultBoard";
 import { classNames } from "../utils/styling";
-import { setDragElement } from "./dragHandler";
+import {
+  getDragElement,
+  getDragElementIndex,
+  setDragElement,
+} from "./dragHandler";
 
 const SIZE = 8;
-
-const [board, setBoard] = createSignal<BoardSquare[]>(defaultBoard);
 // const generated: BoardSquare[] = [];
 // const A_CHAR_CODE = 97;
 // for (let i = SIZE; i > 0; i--) {
@@ -26,11 +34,32 @@ const [board, setBoard] = createSignal<BoardSquare[]>(defaultBoard);
 //   return el;
 // });
 // console.log(newBoard);
-const Chessboard = () => {
+const Chessboard: Component = () => {
+  const [board, setBoard] = createSignal<BoardSquare[]>(defaultBoard);
+  createEffect(() => {
+    console.log(board());
+  });
+  function handleMove(
+    index: number,
+    squareDestination: BoardSquare,
+    destinationIndex: number
+  ) {
+    console.log(board()[index].piece);
+    const piece: Piece = board()[index].piece;
+
+    const newBoard = [...board()];
+
+    newBoard[index] = { ...newBoard[index], piece: squareDestination.piece };
+    newBoard[destinationIndex] = { ...newBoard[destinationIndex], piece };
+    setBoard(newBoard);
+  }
   return (
     <div class="grid aspect-square h-full max-h-full grid-cols-8 shadow">
       <For each={board()}>
-        {(square, i) => <Square index={i()} square={square} />}
+        {(square, i) => {
+          console.log(square);
+          return <Square index={i()} square={square} handleMove={handleMove} />;
+        }}
       </For>
     </div>
   );
@@ -39,6 +68,11 @@ const Chessboard = () => {
 interface SquareProps {
   index: number;
   square: BoardSquare;
+  handleMove: (
+    index: number,
+    squareDestination: BoardSquare,
+    destinationIndex: number
+  ) => void;
 }
 const Square: Component<SquareProps> = (props) => {
   let ref: HTMLDivElement;
@@ -63,20 +97,24 @@ const Square: Component<SquareProps> = (props) => {
     const yOffset =
       e.clientY - imageBoundingRect.height / 2 - imageBoundingRect.y;
 
-    setDragElement(imageRef, xOffset, yOffset);
+    setDragElement(imageRef, props.index, xOffset, yOffset);
   };
   const handleMouseUp = (e: MouseEvent) => {
     console.log("mouseup at " + props.square.position);
+    if (!getDragElement()) return;
+
+    props.handleMove(getDragElementIndex(), props.square, props.index);
   };
   return (
     <div
-      class={classNames(color, "relative")}
+      class={"relative"}
       onMouseDown={handleMouseDown}
       ref={ref}
       onMouseUp={handleMouseUp}
     >
+      {/*Piece Image */}
       {props.square.piece && (
-        <div class="absolute inset-0">
+        <div class="absolute inset-0 z-10">
           <img
             src={`/assets/${props.square.piece.type}_${props.square.piece.color}.png`}
             class="h-full w-full p-2"
@@ -85,6 +123,9 @@ const Square: Component<SquareProps> = (props) => {
           />
         </div>
       )}
+      {/*Tile Color */}
+      <div class={classNames(color, "absolute inset-0 h-full w-full")} />
+      {/*Position Text */}
       <div class="absolute p-1 text-black text-opacity-50">
         {props.square.position}
       </div>
