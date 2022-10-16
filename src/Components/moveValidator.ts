9626cb2c41c5ffeae6d9621aceff61f66bc53dd0
Fernando-J-Garcia <Fernandogar21700@gmail.com>
2022-10-15 19:33:10 -0700

    rework validation logic

--- src/Components/moveValidator.ts
+++ src/Components/moveValidator.ts
@@ -1,56 +1,79 @@
 import { BoardSquare, PieceType } from "../defaultBoard";
 
-function isValidBishop(
-  squareStart: BoardSquare,
-  squareDestination: BoardSquare
-) {
-  console.log(squareStart, squareDestination);
-  const startPosRow = squareStart.position.charAt(0);
-  const startPosCol = squareStart.position.charAt(1);
-  const DestPosRow = squareDestination.position.charAt(0);
-  const DestPosCol = squareDestination.position.charAt(1);
+const possibleMoves = new Map<string, boolean>();
+const BOARD_WIDTH = 8;
 
-  if (
-    squareStart.color !== squareDestination.color ||
-    startPosRow === DestPosRow
-  ) {
+const isInBoundary = (x: number, y: number): Boolean => {
+  console.log(x, y);
+  if (x > 8 || y - 97 > 8 || x < 0 || y < 0) {
     return false;
   }
   return true;
-}
-function isValidKing(squareStart: BoardSquare, squareDestination: BoardSquare) {
-  return true;
-}
-function isValidKnight(
+};
+function validateMove(
   squareStart: BoardSquare,
-  squareDestination: BoardSquare
+  board: BoardSquare[],
+  x: number,
+  y: number
 ) {
-  return true;
+  const pieceType = squareStart.piece.type;
+
+  if (!isInBoundary(x, y)) return false;
+
+  const currentPositionCol = String.fromCharCode(y + 97); //Convert to algabraic format
+  const currentPositionRow = x;
+  const algraicPos = `${currentPositionCol}${currentPositionRow}`;
+
+  //TODO add x and y to get the postion in a one dimensional array
+  const index =
+    BOARD_WIDTH * BOARD_WIDTH - ((y - 1) * BOARD_WIDTH + BOARD_WIDTH + 1 - x);
+  const currentSquare: BoardSquare = board[index];
+
+  console.log({ x, y, index });
+  console.log(currentSquare);
+  if (currentSquare !== squareStart) {
+    //There is no pieces blocking this path
+    if (currentSquare.piece === null) {
+      //move is valid add it to the possible moves
+      possibleMoves.set(algraicPos, true);
+    } else {
+      if (currentSquare.piece.color !== squareStart.color) {
+        possibleMoves.set(algraicPos, true);
+      }
+      return false;
+    }
+  }
+
+  if (pieceType === PieceType.BISHOP) {
+    //check diagonol
+    validateMove(squareStart, board, x + 1, y + 1);
+    validateMove(squareStart, board, x - 1, y - 1);
+    validateMove(squareStart, board, x + 1, y + 1);
+    validateMove(squareStart, board, x - 1, y + 1);
+  }
+}
+
+export function generateAllPossibleMoves(
+  squareStart: BoardSquare,
+  board: BoardSquare[]
+) {
+  possibleMoves.clear(); //clear previous moves
+
+  const startPosRow = squareStart.position.charAt(0);
+  const startPosCol = squareStart.position.charAt(1);
+  let x = startPosRow.charCodeAt(0) - 97 + 1;
+  let y = parseInt(startPosCol);
+
+  //Recursivily go through every move and check if it is valid
+  validateMove(squareStart, board, x, y);
+  console.log(possibleMoves);
 }
 
 export function MoveIsValid(
   squareStart: BoardSquare,
-  squareDestination: BoardSquare
+  squareDestination: BoardSquare,
+  board: BoardSquare[]
 ) {
-  //TODO: PIECE Move rules go here
-  switch (squareStart.piece.type) {
-    case PieceType.BISHOP:
-      if (!isValidBishop(squareStart, squareDestination)) {
-        return false;
-      }
-      break;
-    case PieceType.KING:
-      if (!isValidKing(squareStart, squareDestination)) {
-        return false;
-      }
-      break;
-    case PieceType.KNIGHT:
-      if (!isValidKnight(squareStart, squareDestination)) {
-        return false;
-      }
-      break;
-  }
-
   //is the slot occupied?
   if (squareDestination.piece) {
     //if so, Check if the piece is the same color...
